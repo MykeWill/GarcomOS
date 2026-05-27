902c6f46f38067b6c4862a01c91a26530208f856
Myke William <mykewillow@gmail.com>
2026-05-27 20:30:36 -0300

    feat: adiciona novos campos ao model User no Prisma

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  name!: string;
+
+  @IsEmail()
+  @IsString()
+  email!: string;
+
+  @IsString()
+  password!: string;
+
+  @IsString()
+  role!: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,18 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaService } from '../prisma/prisma.service';
-
 
 @Controller('users')
 export class UsersController {
-  constructor(private prisma: PrismaService) {}
-  
+  constructor(private readonly usersService: UsersService) {}
+
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.prisma.user.create({
-    data: createUserDto,
-  });
+    return this.usersService.create(createUserDto);
   }
 
   @Get()
